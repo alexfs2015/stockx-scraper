094814f2c120535c8e867cfabb3ca7430be73e17
alexfs2015 <44406987+alexfs2015@users.noreply.github.com>
2020-11-04 09:53:05 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from bing_image_downloader import downloader
 
 headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
 
 url = "https://stockx.com/retro-jordans/air-jordan-1?page=1"
-
-
 
 models = []
 for i in range(0, 26):
@@ -17,8 +16,6 @@ for i in range(0, 26):
   for h in soup.find_all(class_="css-1iephdx e1inh05x0"):
     a = h.text
     models.append(a)
-  
+    downloader.download(a, limit=100,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60)
 
-for x in range(0,len(models)):
-  print(models[x])
 print(len(models))
