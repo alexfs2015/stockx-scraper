b3e8eb7cd56632d1a52e8214ba8b969acffa7625
alexfs2015 <44406987+alexfs2015@users.noreply.github.com>
2020-11-04 09:39:16 +0100

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+
+url = "https://stockx.com/retro-jordans/air-jordan-1?page=1"
+
+
+
+models = []
+for i in range(0, 26):
+  url_search = url[:-1]
+  url_search = url_search + str(i)
+  r = requests.get(url_search, headers=headers)
+  soup = BeautifulSoup(r.text, 'html.parser')
+
+  for h in soup.find_all(class_="css-1iephdx e1inh05x0"):
+    a = h.text
+    models.append(a)
+  
+
+for x in range(0,len(models)):
+  print(models[x])
+print(len(models))
